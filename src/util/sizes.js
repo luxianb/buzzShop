82f31867ac458306c2bbb91d90bffbe4615e4d0e
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-15 18:59:54 +0800

    Add utility file for app sizing

---
+++ src/util/sizes.js
@@ -0,0 +1,20 @@
+
+const baseFontSize = 16;
+export const fontSize = {
+  XXL: baseFontSize * 2,
+  XL: baseFontSize * 1.5,
+  L: baseFontSize * 1.17,
+  M: baseFontSize,
+  S: baseFontSize * .83,
+  XS: baseFontSize * .67
+}
+
+const baseGapSize = 12;
+export const gap = {
+  XXL: baseGapSize * 3,
+  XL: baseGapSize * 2,
+  L: baseGapSize * 1.5,
+  M: baseGapSize,
+  S: baseGapSize * .5,
+  XS: baseGapSize *.25,
+}
