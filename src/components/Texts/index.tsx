970eac12fbfb4ecbc8c4225523555d45b314bb62
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-15 18:59:08 +0800

    Initial Commit - Text component

---
+++ src/components/Texts/index.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import {Text} from 'react-native';
+import {fontSize} from 'util/sizes';
+
+const textBase = styled(Text)`
+  margin: 0;
+`;
+
+const headerBase = styled(textBase)`
+  font-weight: 800;
+`;
+
+export const H1 = styled(headerBase)`
+  font-size: ${fontSize.XXL}px;
+`;
+export const H2 = styled(headerBase)`
+  font-size: ${fontSize.XL}px;
+`;
+export const H3 = styled(headerBase)`
+  font-size: ${fontSize.L}px;
+`;
+export const H4 = styled(headerBase)`
+  font-size: ${fontSize.M}px;
+`;
+export const H5 = styled(headerBase)`
+  font-size: ${fontSize.S}px;
+`;
+export const H6 = styled(headerBase)`
+  font-size: ${fontSize.XS}px;
+`;
+export const P = styled(textBase)`
+  font-size: ${fontSize.M}px;
+`;
