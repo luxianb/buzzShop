ec73f8f5ea3c7b6570d577bd7a644fff9a39edf0
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-22 17:03:08 +0800

    Add component index file

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {Row, Col, Page, Hr} from './Views';
+export {P, H1, H2, H3, H4, H5, H6, Prompt, A} from './Texts';
+export {default as Button} from './Buttons';
+export {default as Modal} from './Modals'
