cb78c638782092b4ba27f285c3488f099277b8b8
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-22 17:00:41 +0800

    Inputs - Add label component

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {TextInput, View, Pressable} from 'react-native';
+import {TextInput, Pressable} from 'react-native';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
@@ -7,19 +7,22 @@ import {gap} from 'util/sizes';
 import {Row} from 'components/Views'
 import color from 'util/colors';
 import { hexToRGB } from 'util/helperFunctions';
+import { H3 } from 'components/Texts';
 
-export const Input = styled(TextInput)`
+export const Input = styled(TextInput).attrs({
+  placeholderTextColor: color.blueGrey[400]
+  })`
   border-width: 1px;
-  border-color: ${(prop: any) => prop.error ? color.red['A200'] : 'transparent' };
+  border-color: ${(prop: {error?: boolean}) => prop.error ? color.red['A200'] : 'transparent' };
   border-radius: ${gap.M}px;
   padding: ${gap.M}px;
   background-color: ${hexToRGB(color.blueGrey[900], .05)};
 `;
 
 
-const PseudoInputContainer = styled(Row)`
+export const PseudoInputContainer = styled(Row)`
   border-width: 1px;
-  border-color: ${(props: any) => props.error ? color.red['A200'] : 'transparent' };
+  border-color: ${(props: {error?: boolean}) => props.error ? color.red['A200'] : 'transparent' };
   /* border-color: ${(prop: any) => prop.error ? color.red['A200'] : color.blueGrey[400] }; */
   padding: ${gap.M}px;
   justify-content: space-between;
@@ -35,15 +38,20 @@ export const PasswordInput = (props: any) => {
       <TextInput
         {...props}
         secureTextEntry={blur}
-        style={[{padding: 0}]}
+        style={[{padding: 0, flex: 1}]}
+        placeholderTextColor={color.blueGrey[400]}
       />
       <Pressable onPress={() => setBlur(!blur)}>
         {blur ? (
           <FontAwesomeIcon icon={faEyeSlash} style={{opacity: .6}}/>
-          ) : (
-            <FontAwesomeIcon icon={faEye} style={{opacity: .8}}/>
+        ) : (
+          <FontAwesomeIcon icon={faEye} style={{opacity: .8}}/>
         )}
       </Pressable>
     </PseudoInputContainer>
   )
 }
+
+export const Label = styled(H3)`
+  margin-bottom: ${gap.S}px;
+`;
