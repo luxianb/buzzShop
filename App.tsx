31bfbfacd1a87688e4c9dc91b262e8d7e22ead6f
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-22 17:14:52 +0800

    Update navigation router

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar, useColorScheme} from 'react-native';
 import {Provider} from "react-redux"
 import { NavigationContainer } from '@react-navigation/native';
@@ -6,6 +7,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Landing from 'pages/Landing';
 import SignUp from 'pages/SignUp';
 import store from 'util/redux/store';
+import LoginPage from 'pages/Login';
+import ProfilePage from 'pages/Profile';
+import FindProducts from 'pages/FindProducts';
+import CartPage from 'pages/Cart';
+import PostProductPage from 'pages/PostProduct';
+import ProductDetailPage from 'pages/ProductDetail';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,13 +25,21 @@ const App = () => {
 
   return (
     <Provider store={store}>
+      <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <Stack.Navigator initialRouteName="SignUp" screenOptions={{headerShown: false}}>
-          <Stack.Screen name="SignUp" component={SignUp}  />
-          <Stack.Screen name="Landing" component={Landing}  />
+        <Stack.Navigator initialRouteName="Landing" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Landing" component={Landing} />
+          <Stack.Screen name="Login" component={LoginPage} />
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="AddProduct" component={PostProductPage} />
+          <Stack.Screen name="Profile" component={ProfilePage} />
+          <Stack.Screen name="Search" component={FindProducts} />
+          <Stack.Screen name="Cart" component={CartPage} />
+          <Stack.Screen name="ProductDetail" component={ProductDetailPage} />
         </Stack.Navigator>
       </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
     // <SafeAreaView style={backgroundStyle}>
     //   <ScrollView
