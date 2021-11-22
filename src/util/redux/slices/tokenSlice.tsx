9432b841c8556566923d6754f9a4b8ed5fb477af
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-22 16:59:40 +0800

    Add reducer for storing login info

---
+++ src/util/redux/slices/tokenSlice.tsx
@@ -0,0 +1,32 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+  access: '',
+  userInfo: {
+    username: '',
+    id: '',
+    email: '',
+  }
+}
+
+export const tokenSlice = createSlice({
+  name: 'token',
+  initialState,
+  reducers: {
+    setToken: (state, action) => {
+      state.access = action.payload
+    },
+    setLoginInfo: (state: any, action: any) => {
+      state.access = action.payload.access,
+      state.userInfo = action.payload.userInfo
+    },
+    removeLoginInfo: (state: any) => {
+      state.access = initialState.access,
+      state.userInfo = initialState.userInfo
+    }
+  }
+})
+
+export const { setToken, setLoginInfo, removeLoginInfo } = tokenSlice.actions
+
+export default tokenSlice.reducer;
--- src/util/redux/store.tsx
+++ src/util/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/counterSlice';
+import tokenReducer from './slices/tokenSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer
+    counter: counterReducer,
+    token: tokenReducer
   }
 })
 
