8ff203b21777aeaac80a38e03e3043cd77aefd0a
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-24 17:18:15 +0800

    Styled search bar component

--- src/components/Inputs/SearchBar.tsx
+++ src/components/Inputs/SearchBar.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {TextInput, Pressable} from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import { PseudoInputContainer } from ".";
+import {gap} from 'util/sizes'
 import color from 'util/colors'
 
 export default function SearchBar(props: any) {
   return(
-    <PseudoInputContainer>
+    <PseudoInputContainer style={props.style}>
+      <FontAwesomeIcon icon={faSearch} style={{color: color.blueGrey[600], marginRight: gap.M}} />
       <TextInput
         {...props}
         style={[{padding: 0, flex: 1}]}
         placeholderTextColor={color.blueGrey[400]}
+        placeholder="Search for..."
       />
     </PseudoInputContainer>
   );
