4cad7f81a30826de3e545bb77fa1d23beb0fa7ec
luxianb <40238895+luxianb@users.noreply.github.com>
2021-11-24 22:30:38 +0800

    File tidy up

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar, useColorScheme} from 'react-native';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {Provider} from "react-redux"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import store from 'util/redux/store';
 import {
   Landing,
@@ -31,28 +33,28 @@ const App = () => {
     <Provider store={store}>
       <SafeAreaProvider>
         <NavigationContainer>
+
           <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
           <Stack.Navigator initialRouteName="Landing" screenOptions={{headerShown: false}}>
             <Stack.Screen name="Landing" component={Landing} />
+            {/* Auth pages */}
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="SignUp" component={SignUp} />
+            {/* Product Pages */}
             <Stack.Screen name="AddProduct" component={PostProductPage} />
-            <Stack.Screen name="Profile" component={ProfilePage} />
             <Stack.Screen name="Search" component={FindProducts} />
+            <Stack.Screen name="ProductDetail" component={ProductDetailPage} />
+            {/* Cart */}
             <Stack.Screen name="Cart" component={CartPage} />
             <Stack.Screen name="CheckOut" component={CheckOutPage} />
-            <Stack.Screen name="ProductDetail" component={ProductDetailPage} />
+            {/* Profile */}
+            <Stack.Screen name="Profile" component={ProfilePage} />
           </Stack.Navigator>
+
         </NavigationContainer>
       </SafeAreaProvider>
     </Provider>
-    // <SafeAreaView style={backgroundStyle}>
-    //   <ScrollView
-    //     contentInsetAdjustmentBehavior="automatic"
-    //     style={backgroundStyle}>
-    //     <View />
-    //   </ScrollView>
-    // </SafeAreaView>
   );
 };
 
